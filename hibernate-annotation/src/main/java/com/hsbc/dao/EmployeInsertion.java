89786dbf80dacada77bd3a5aa58c96fb1ec66348
Bapun <paridabapun@gmail.com>
2018-06-04 20:22:27 +0530

    class name change

--- hibernate-annotation/src/main/java/com/hsbc/dao/EmployeInsertion.java
+++ hibernate-annotation/src/main/java/com/hsbc/dao/EmployeInsertion.java
@@ -7,15 +7,13 @@ import org.hibernate.cfg.Configuration;
 
 import com.hsbc.domain.Employee;
 
-public class DbInteraction {
+public class EmployeInsertion {
 public static void main(String[] args) {
 	
 	Configuration configuration=new Configuration().configure("com/hsbc/conf/hibernate.cfg.xml");
 	SessionFactory factory=configuration.buildSessionFactory();
 	Session session=factory.openSession();
-	
 	Transaction transaction=session.beginTransaction();
-	
 	Employee employee=new Employee();
 	employee.setEmployee_id(2);
 	employee.setFirstname("Pooja ");
