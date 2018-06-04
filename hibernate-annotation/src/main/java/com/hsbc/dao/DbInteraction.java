3b147b103b8b2a961a11b613ae493bdc2400bf20
Bapun <paridabapun@gmail.com>
2018-06-04 19:55:22 +0530

    hibernate examples

---
+++ hibernate-annotation/src/main/java/com/hsbc/dao/DbInteraction.java
@@ -0,0 +1,30 @@
+package com.hsbc.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.hsbc.domain.Employee;
+
+public class DbInteraction {
+public static void main(String[] args) {
+	
+	Configuration configuration=new Configuration().configure("com/hsbc/conf/hibernate.cfg.xml");
+	SessionFactory factory=configuration.buildSessionFactory();
+	Session session=factory.openSession();
+	
+	Transaction transaction=session.beginTransaction();
+	
+	Employee employee=new Employee();
+	employee.setEmployee_id(2);
+	employee.setFirstname("Pooja ");
+	employee.setLastname("Ahuja ");
+	session.save(employee);
+	transaction.commit();
+	
+	
+	
+
+}
+}
---
+++ hibernate-annotation/src/main/java/com/hsbc/domain/Employee.java
@@ -0,0 +1,42 @@
+package com.hsbc.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+	@Id()
+	@Column(name ="eid")
+	private int employee_id;
+	private String firstname;
+	private String lastname;
+
+	public int getEmployee_id() {
+		return employee_id;
+	}
+
+	public void setEmployee_id(int employee_id) {
+		this.employee_id = employee_id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+}
